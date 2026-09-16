26d5204307b9cc74302d97173b111f26083b30bb
mizuochi <63951629+KuonjiUkyo@users.noreply.github.com>
2026-09-16 09:35:07 +0900

    Update RAW_REPOSITORY URL to new source

--- app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
+++ app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
@@ -139,7 +139,7 @@ class PayloadRepository(private val context: Context) {
         private const val COMMIT_API_URL =
             "https://api.github.com/repos/BuSung-dev/Root-My-Galaxy-Payloads/git/ref/heads/main"
         private const val RAW_REPOSITORY =
-            "https://raw.githubusercontent.com/BuSung-dev/Root-My-Galaxy-Payloads"
+            "https://raw.githubusercontent.com/KuonjiUkyo/Root-My-Galaxy-Payloads"
         private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/main/"
         private const val MAX_COMMIT_RESPONSE_BYTES = 16 * 1024
         private const val MAX_MANIFEST_BYTES = 256 * 1024
